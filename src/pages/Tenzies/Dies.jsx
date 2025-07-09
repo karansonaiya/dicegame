8b2e304e1562534f1bd7ca24843271b44f787ca8
karansonaiya <sonaiyakaran339@gmail.com>
2025-07-09 10:13:16 +0530

    feat: add win condition and disable button when game is won

--- src/pages/Tenzies/Dies.jsx
+++ src/pages/Tenzies/Dies.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 
-
-const Dies = ({dice , toggleHold}) => {
+const Dies = ({ dice, toggleHold, win }) => {
     return (
-        <>
-            <div className="flex flex-wrap justify-center items-center gap-2 mt-6">
-                {dice.map((die, index) => (
-                    <Button
-                        key={index}
-                        onClick={() => toggleHold(index)}
-                        className={`flex justify-center bg-amber-300 text-blue-900 mx-auto items-center w-[15%] h-12 rounded-lg text-2xl font-bold cursor-pointer transition ${die.isHeld ? 'bg-green-600' : 'hover:bg-yellow-600'
-                            }`}
-                    >
-                        {die.value}
-                    </Button>
-                ))}
-            </div>
-        </>
+        <div className="flex flex-wrap justify-center items-center gap-2 mt-6">
+            {dice.map((die, index) => (
+                <Button
+                    key={index}
+                    onClick={() => toggleHold(index)}
+                    disabled={win} 
+                    className={`flex justify-center text-blue-900 mx-auto items-center w-[15%] h-12 rounded-lg text-2xl font-bold cursor-pointer transition 
+                        ${die.isHeld ? 'bg-green-600' : 'bg-amber-300 hover:bg-yellow-600'}
+                        ${win ? 'opacity-50 cursor-not-allowed' : ''}`} 
+                >
+                    {die.value}
+                </Button>
+            ))}
+        </div>
     )
 }
 
